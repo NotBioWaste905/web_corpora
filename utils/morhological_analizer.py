9231d9fdaaa81e50184f6e78e0ab4616e9ce6a2f
gobova <44808696+gobova@users.noreply.github.com>
2022-10-26 19:16:45 +0300

    Add files via upload
    this file creates a column or a separate scv-file with meta-information for each word in the reviews (normal form, source form, and part of speech).

--- utils/morhological_analizer.py
+++ utils/morhological_analizer.py
@@ -7,37 +7,41 @@ python -m spacy download en_core_web_sm
 import pandas
 import spacy
 from spacy.lang.en import English
-
+#создается датафрейм corpus (копия того, что ангелина сделала)
 corpus = pandas.read_csv('corpus.csv')
 
 # добавление пустого столбца в датафрейм, название столбца "meta"
 corpus['meta'] = ''
 
-# The Sentencizer component is a pipeline component that splits sentences on punctuation like ., ! or ?.
+# Компонент Sentencizer - это компонент, который разделяет предложения по знакам препинания типа ., ! или ?
 sentencizer = English()
 sentencizer.add_pipe("sentencizer")
 
-# POS таггер
+# подключение POS тэггер
 tagger = spacy.load("en_core_web_sm")
 
-for index in range(10, len(corpus)):
+
+for index in range(0, len(corpus)):
     row = corpus.iloc[index]
-    text = row['reviews'] #заменить на review
+    text = row['review']
     sentences = [sent.text for sent in sentencizer(text).sents]
     meta_text = []
     for sentence in sentences:
         doc = tagger(sentence)
+        # в массиве meta_sentence будет массив из pos, normal original form для поиска
         meta_sentence = []
         for token in doc:
             # for each word in sentence meta information is pos, normal form, original form
             # для поиска по части речи
             pos = token.pos_
-            # для поиса по словоформе
+            # для поиска по словоформе
             normal_form = token.lemma_
             original_form = token.text
             meta = (pos, normal_form, original_form)
             meta_sentence.append(meta)
         meta_text.append(meta_sentence)
     corpus['meta'][index] = meta_text
-    print(corpus.iloc[index])
-    break
+
+# новая таблица meta с одним столбцом
+meta = pandas.DataFrame(corpus['meta'])
+print(meta.head())
